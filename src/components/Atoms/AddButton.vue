5d7ed7441f05a9b900f74b07b0b791540dc4f2e5
河村 泰佑 <kata.kawa813@gmail.com>
2021-06-25 23:59:34 +0900

    add

---
+++ src/components/Atoms/AddButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <button @click="push">Click Button</button>
+  </div>
+</template>
+<script lang="ts">
+import Component from "vue-class-component";
+import { Emit, Vue } from "vue-property-decorator";
+
+@Component
+export default class AddButton extends Vue {
+  @Emit("push")
+  public push(): void {
+    // no return
+  }
+}
+</script>
--- src/components/CountNumber.vue
+++ src/components/CountNumber.vue
@@ -7,7 +7,14 @@
 <script lang="ts">
 import Componrnt from "vue-class-component";
 import { Vue } from "vue-property-decorator";
+import AddButton from "./components/Atoms/AddButton.vue";
+import Display from "./components/Atoms/Display.vue";
 
-@Componrnt({})
+@Componrnt({
+  components: {
+    AddButton,
+    Display,
+  },
+})
 export default class App extends Vue {}
 </script>
